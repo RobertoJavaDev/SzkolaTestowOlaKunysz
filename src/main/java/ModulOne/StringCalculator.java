298d7a14e010d72b33da6a727620a69c1835ff09
RobertoJavaDev <robertojavadev@gmail.com>
2021-03-19 21:37:45 +0100

    implemented code in StingCalculator class

--- src/main/java/ModulOne/StringCalculator.java
+++ src/main/java/ModulOne/StringCalculator.java
@@ -1,4 +1,24 @@
 package ModulOne;
 
 public class StringCalculator {
+
+    int add(String input) {
+
+        if (input.isEmpty()) {
+            return 0;
+        }
+
+        String[] numbers = input.split(",");
+
+        if (numbers.length == 1) {
+            String number = numbers[0];
+            return getIntFrom(number);
+        }
+        return getIntFrom(numbers[0] + numbers[1]);
+    }
+
+    private int getIntFrom(String number) {
+        return Integer.parseInt(number);
+    }
 }
+
