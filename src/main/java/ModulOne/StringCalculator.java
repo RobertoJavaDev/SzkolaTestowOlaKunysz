713e69df703341ccfe0f9f0965713b600af3d336
RobertoJavaDev <robertojavadev@gmail.com>
2021-03-19 22:49:05 +0100

    refactoring StringCalculator class

--- src/main/java/ModulOne/StringCalculator.java
+++ src/main/java/ModulOne/StringCalculator.java
@@ -13,8 +13,8 @@ public class StringCalculator {
         if (numbers.length == 1) {
             String number = numbers[0];
             return getIntFrom(number);
-        }
-        return getIntFrom(numbers[0] + numbers[1]);
+        }else
+        return getIntFrom(numbers[0]) + getIntFrom(numbers[1]) + getIntFrom(numbers[2]) + getIntFrom(numbers[3]);
     }
 
     private int getIntFrom(String number) {
