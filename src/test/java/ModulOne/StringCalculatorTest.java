1d607bbe706f308947b702ec9669fa02ad1fe493
RobertoJavaDev <robertojavadev@gmail.com>
2021-03-19 22:49:38 +0100

    added unit tests to StingCalculator class

--- src/test/java/ModulOne/StringCalculatorTest.java
+++ src/test/java/ModulOne/StringCalculatorTest.java
@@ -1,7 +1,39 @@
 package ModulOne;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator calculator;
 
+    @BeforeEach
+    public void setUp(){
+    createCalculator();
+    }
+
+    @Test
+    void shouldSumWhenNotNumberGiven() {
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    void shouldSumFourNumbersWhenFourPrimeNumbersGiven() {
+        assertEquals(17,calculator.add("2,3,5,7"));
+    }
+
+    @Test
+    void shouldSumNumbersWhenAnyOfNumberIsNegative() {
+        assertEquals(7,calculator.add("1,-3,4,5"));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAlphabetCharacterGiven() {
+        assertThrows(NumberFormatException.class, ()-> calculator.add("a,b,c,d"));
+    }
+
+    private void createCalculator() {
+        calculator = new StringCalculator();
+    }
 }
