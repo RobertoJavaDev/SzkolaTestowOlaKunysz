2ed871b8cec7483b640a2874bc6cafc38b6ee688
RobertoJavaDev <robertojavadev@gmail.com>
2021-03-20 22:28:09 +0100

    Refactored Product class

--- src/main/java/ModuleTwo/Product.java
+++ src/main/java/ModuleTwo/Product.java
@@ -4,10 +4,12 @@ public class Product {
 
     int id;
     double netPrice;
+    String type;
 
-    public Product (int id, double netPrice){
+    public Product (int id, double netPrice, String type){
         this.id = id;
         this.netPrice = netPrice;
+        this.type = type;
     }
 
     public int getId() {
@@ -24,5 +26,13 @@ public class Product {
 
     public void setNetPrice(double netPrice) {
         this.netPrice = netPrice;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
