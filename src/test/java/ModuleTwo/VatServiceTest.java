c8a2891eb1cf0ff92aa1699eef9fc7a228b1c1be
RobertoJavaDev <robertojavadev@gmail.com>
2021-03-20 22:29:35 +0100

    Modified test class

--- src/test/java/ModuleTwo/VatServiceTest.java
+++ src/test/java/ModuleTwo/VatServiceTest.java
@@ -2,37 +2,45 @@ package ModuleTwo;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VatServiceTest {
 
-    VatService vatService = new VatService();
-    private Product generateProduct(int id, double netPrice){
-        return new Product(id,netPrice);
+    VatService vatService;
+    VatProvider vatProvider;
+
+    private Product generateProduct(int id, double netPrice, String type) {
+        return new Product(id, netPrice, type);
     }
 
     @Test
     void shouldCalculateVatValueForDefaultVat() throws Exception {
         //given
-        Product product = generateProduct(1,100);
+        Product product = generateProduct(1, 100, "Groceries");
 
         //when
         double result = vatService.getGrossPriceForDefaultVat(product);
 
         //then
-        assertEquals(123,result);
+        assertEquals(123, result);
     }
 
-    @Test
-    void shouldCalculateGrossPriceForDifferentVatValue() throws Exception {
-        //given
-
-        //when
-        double result = vatService.getGrossPriceForDifferentVat(100,0.1);
-
-        //then
-        assertEquals(110,result);
-
+    //    @Test
+//    void shouldCalculateGrossPriceForDifferentVatValue() throws Exception {
+//        //given
+//
+//        //when
+//        double result = vatService.getGrossPriceForDifferentVat(100,0.1);
+//
+//        //then
+//        assertEquals(110,result);
+//
+//    }
+    @BeforeEach
+    void prepareVatService() {
+        vatProvider = Mockito.mock(VatProvider.class);
+        vatService = new VatService(vatProvider);
     }
 }
