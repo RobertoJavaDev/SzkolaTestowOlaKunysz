564a5ed356ec59c7b7308951431420f39bbcfb2a
RobertoJavaDev <robertojavadev@gmail.com>
2021-04-25 10:16:11 +0200

    added method getScore and roll

--- src/main/java/ModuleThree/BowlingKata.java
+++ src/main/java/ModuleThree/BowlingKata.java
@@ -3,10 +3,12 @@ package ModuleThree;
 public class BowlingKata {
     int score = 0;
 
-    public int add(int roll, int pins) {
-        for (int i = 0; i <roll; i++) {
-            score += pins;
-        }
-        return score;
-    }
+     public int getScore(){
+         return score;
+     }
+
+     public void roll(int pins){
+         score += pins;
+     }
+
 }
