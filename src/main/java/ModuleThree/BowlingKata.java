2429602fd72c7fbf4f58b35b09a447112e62f102
RobertoJavaDev <robertojavadev@gmail.com>
2021-04-25 09:19:14 +0200

    added method add

--- src/main/java/ModuleThree/BowlingKata.java
+++ src/main/java/ModuleThree/BowlingKata.java
@@ -1,4 +1,12 @@
 package ModuleThree;
 
 public class BowlingKata {
+    int score = 0;
+
+    public int add(int roll, int pins) {
+        for (int i = 0; i <roll; i++) {
+            score += pins;
+        }
+        return score;
+    }
 }
