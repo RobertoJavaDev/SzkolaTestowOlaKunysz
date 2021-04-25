43f52e6337c6deb2138950caa775b784c7b70d96
RobertoJavaDev <robertojavadev@gmail.com>
2021-04-25 14:23:12 +0200

    added function which check Spare in getSocre method

--- src/main/java/ModuleThree/BowlingKata.java
+++ src/main/java/ModuleThree/BowlingKata.java
@@ -1,14 +1,30 @@
 package ModuleThree;
 
 public class BowlingKata {
-    int score = 0;
+
+    int [] rolls = new int[21];
+    int roll;
 
      public int getScore(){
+         int cursor = 0;
+         int score = 0;
+
+         for (int i = 0; i < 20; i++) {
+             if (rolls[cursor] + rolls[cursor+1] == 10){
+                 score += 10 + rolls[cursor+2];
+                 cursor += 2;
+                 i++;
+             }
+             else{
+                 score += rolls[i];
+                 cursor ++;
+             }
+         }
          return score;
      }
 
      public void roll(int pins){
-         score += pins;
+         rolls[roll++] += pins;
      }
 
 }
