1f17bdc1a8a07a2dc962daa5bb4c1dd8f33c0485
RobertoJavaDev <robertojavadev@gmail.com>
2021-05-03 13:51:09 +0200

    modified Check method for numbers divisible by 3 and 5

--- src/main/java/ModuleThree/FizzBuzz.java
+++ src/main/java/ModuleThree/FizzBuzz.java
@@ -9,13 +9,17 @@ public class FizzBuzz {
 
         System.out.println(numberTest);
 
-        if (numberTest % 3 == 0){
+        if (numberTest % 3 == 0 && numberTest % 5 != 0){
             System.out.println(FIZZ);
             return FIZZ;
         }
-        else if (numberTest % 5 == 0){
+        else if (numberTest % 5 == 0 && numberTest % 3 != 0){
             System.out.println(BUZZ);
             return BUZZ;
+        }
+        else if (numberTest % 15 == 0){
+            System.out.println("FizzBuzz");
+            return "FizzBuzz";
         }
         return null;
     }
