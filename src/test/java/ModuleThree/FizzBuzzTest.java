64139e553f7743fd71eceab031625c581cde5845
RobertoJavaDev <robertojavadev@gmail.com>
2021-05-03 13:24:48 +0200

    added new test - number divisible by three

--- src/test/java/ModuleThree/FizzBuzzTest.java
+++ src/test/java/ModuleThree/FizzBuzzTest.java
@@ -1,7 +1,21 @@
 package ModuleThree;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 
+    @Test
+    void shouldReturnFizzWhenNumberDivisibleByThree() {
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.check(9);
+
+        //then
+        assertThat(result).contains("Fizz");
+
+    }
 }
