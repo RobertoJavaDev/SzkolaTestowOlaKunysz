6de38f89e3c676203672faa0db0726796ab4de5b
RobertoJavaDev <robertojavadev@gmail.com>
2021-05-26 09:13:52 +0200

    added new test sorted list by age of employees

--- src/test/java/ModuleBonus/EmployeeReportTest.java
+++ src/test/java/ModuleBonus/EmployeeReportTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class EmployeeReportTest {
@@ -18,5 +19,23 @@ class EmployeeReportTest {
 
         //then
         Assertions.assertThat(employeeList.size()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldReturnEmployeesListWhichAreOlderThan18Years() {
+        //given
+        EmployeeReport employeeReport = new EmployeeReport();
+        List<Employee> employees = Arrays.asList(
+                new Employee("Max",17),
+                new Employee("Sepp", 18),
+                new Employee("Nina",15),
+                new Employee("Mike",51));
+
+        //when
+        List<Employee> employeeList = employeeReport.getEmployees(employees);
+
+        //then
+        Assertions.assertThat(employeeList.size()).isEqualTo(2);
+
     }
 }
