ad97cc80d625c2898efec04defa6b3fd5db86c08
RobertoJavaDev <robertojavadev@gmail.com>
2021-05-27 21:23:09 +0200

    added sorted to stream in getEmployees method

--- src/main/java/ModuleBonus/EmployeeReport.java
+++ src/main/java/ModuleBonus/EmployeeReport.java
@@ -1,18 +1,19 @@
 package ModuleBonus;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EmployeeReport {
 
-    public List<Employee> getEmployees(List<Employee> employees) {
+    public List<Employee> getEmployees(List<Employee> employees, boolean descending) {
         return employees.stream()
                 .filter(employee -> isAdult(employee))
+                .sorted(descending ? Comparator.comparing(Employee::getName).reversed() : Comparator.comparing(Employee::getName))
                 .collect(Collectors.toList());
     }
 
     private boolean isAdult(Employee employee) {
         return employee.getAge() > 17;
-
     }
 }
